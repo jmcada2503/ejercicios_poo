adfe66eb6b7d95f70b62fddff3f01c6d342a5cf1
Juan Miguel <juanmiscj25@gmail.com>
2023-06-18 23:50:57 -0500

    moved ejercicio2 to p2

---
+++ actividad5/p2/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import figuras.Cilindro;
+import figuras.Esfera;
+import figuras.Piramide;
+
+import interfaz.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
+
+    }
+
+}

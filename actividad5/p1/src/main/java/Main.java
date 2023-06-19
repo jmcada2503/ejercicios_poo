4481455bc5d2b7dbc2ca92953c163f435e408320
Juan Miguel <juanmiscj25@gmail.com>
2023-06-18 22:44:07 -0500

    moved ejercicio1 to p1

---
+++ actividad5/p1/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import interfaz.MainWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
+
+    }
+
+}
